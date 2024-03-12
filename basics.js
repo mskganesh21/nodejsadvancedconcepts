1b970a22f97dc7c3d26bb9b16daf4b940f01d09f
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-03-12 18:25:32 +0530

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,46 @@
+/* archietecture design
+structure of the application
+scalability 
+reliability
+maintainability
+*/
+
+/* middleware development
+authentication, authorization,
+logging,caching and error handling
+*/
+
+/* data modelling
+schemas,relationships,efficient data storage
+and retrieval
+*/
+
+/* perfomance optimisation
+handling concurrent users
+*/
+
+/* security considerations
+sql injection, xss (cross site scripting)
+cross-site-request-forgery(crsf)
+*/
+
+/* caching strategies
+in memory caching and cdn's
+*/
+
+/* background jobs and task queues
+sending emails,generating reports, or handling large data imports.
+Task queues like RabbitMQ or redis can be useful for this purpose
+*/
+
+/* Testing
+TDD
+*/
+
+/*Logging and Monitoring
+
+*/
+
+/* Documentation
+
+*/
